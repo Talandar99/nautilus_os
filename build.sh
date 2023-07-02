8403facf4db386be9d9ae74134172f1f2fe4caf6
Talandar <talandar66@protonmail.com>
2023-07-02 18:23:56 +0200

    removed kernel from build instructions

--- build.sh
+++ build.sh
@@ -2,30 +2,10 @@
 
 #Settings
 KERNEL_VERSION=6.3.2
-SYSTEM_VERSION=0.1.0
+SYSTEM_VERSION=0.2.0
 # make distro directory 
 mkdir nautilus
 cd nautilus || exit
-
-##################################
-# Kernel
-##################################
-echo "Getting kernel"
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VERSION.tar.xz
-tar xf linux-$KERNEL_VERSION.tar.xz
-cd linux-$KERNEL_VERSION || exit
-# configure kernel (default)
-echo "creating configuration"
-make defconfig
-# compile kernel
-echo "compiling kernel"
-make -j$(nproc)
-# install kernel in project directory 
-echo "installing kernel"
-mkdir ../kernel-bin
-make INSTALL_PATH=../kernel-bin install
-cd ..
-
 ##################################
 # create filesystem
 ##################################
@@ -96,8 +76,6 @@ mv curl rootfs/bin/curl
 ##################################
 echo "creating Dockerfile"
 echo "FROM scratch" > Dockerfile
-# add kernel
-echo "COPY kernel-bin/vmlinuz /" >> Dockerfile
 # add filesystem
 echo "COPY rootfs/ /" >> Dockerfile
 # default shell
